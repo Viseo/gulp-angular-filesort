2964f0ba0af8ab67fd13a81f47cc91e0e5b18295
Sebastien P <sp@fing.pro>
2017-10-29 14:03:52 +0100

    Be more consistent regarding arrow and regular functions

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function createSorter(order = []) {
     return _.clamp(i, 0, maxOrder);
   }
 
-  return (a, b) => {
+  return function sorter(a, b) {
     a = getPath(a);
     b = getPath(b);
     // Sort by user patterns or alphabetically
@@ -29,7 +29,7 @@ function createSorter(order = []) {
   };
 }
 
-module.exports = function (options = {}) {
+module.exports = function gulpAngularFilesort(options = {}) {
   // Create a sorter function for user patterns
   const sorter = createSorter(options.attachmentsOrder);
 
@@ -38,15 +38,11 @@ module.exports = function (options = {}) {
   // Stores modules graph ([moduleFilePath: string, parentModuleId: string])
   const graph = [];
   // Stores modules ({ file: File[], files: File[] }) by id
-  const idsMap = Object.create(null);
+  const byIds = Object.create(null);
   // Stores modules ({ file: File[], files: File[] }) by file path
-  const pathsMap = Object.create(null);
+  const byPaths = Object.create(null);
   // Stores standalone modules (no dependencies, not depended on)
-  const standaloneMap = Object.create(null);
-
-  function getModule(id) {
-    return idsMap[id] || (idsMap[id] = { file: [], files: [] });
-  }
+  const standalone = Object.create(null);
 
   function transform(file, encoding, next) {
     const error = err => this.emit('error', new PluginError(PLUGIN_NAME, err));
@@ -84,13 +80,16 @@ module.exports = function (options = {}) {
       return;
     }
 
+    // Get module by id, storing it if nececessary
+    const getModule = id => byIds[id] || (byIds[id] = { file: [], files: [] });
+
     // Add modules to the maps
     _.each(modules, (dependencies, id) => {
-      const module = pathsMap[file.path] = getModule(id);
+      const module = byPaths[file.path] = getModule(id);
       // The module has no dependencies so it is standalone
-      if (_.isEmpty(dependencies)) { standaloneMap[id] = module; }
+      if (_.isEmpty(dependencies)) { standalone[id] = module; }
       // In case the module is declared again with dependencies
-      else { delete standaloneMap[id]; }
+      else { delete standalone[id]; }
       // Store declaration files (modules should be defined once but...)
       module.file.push(file);
     });
@@ -103,7 +102,7 @@ module.exports = function (options = {}) {
       if (_.some(modules, dependencies => _.includes(dependencies, id))) {
         graph.push([file.path, id]);
         // The module is depended on so it is not standalone
-        delete standaloneMap[id];
+        delete standalone[id];
       }
       // If not, then it is an attachment to an existing module
       else { getModule(id).files.push(file); }
@@ -113,7 +112,7 @@ module.exports = function (options = {}) {
   }
 
   function flush(next) {
-    _.chain(idsMap)
+    _.chain(byIds)
       // Get not declared modules
       .filter(module => _.isEmpty(module.file))
       // Get their attached files
@@ -127,7 +126,7 @@ module.exports = function (options = {}) {
 
     _.chain(graph)
       // Map graph sring items to module objects
-      .map(tuple => [pathsMap[tuple[0]], idsMap[tuple[1]]])
+      .map(tuple => [byPaths[tuple[0]], byIds[tuple[1]]])
       // Remove circular dependencies keeping only first occurrences
       .uniqWith((a, b) => a[0] === b[1] && a[1] === b[0])
       // Sort modules by dependencies
@@ -135,7 +134,7 @@ module.exports = function (options = {}) {
       // Remove unknown/external dependencies
       .compact()
       // Add standalone modules to the chain
-      .concat(_.values(standaloneMap))
+      .concat(_.values(standalone))
       // Get modules declaration and attached files sorted by user patterns
       .flatMap(module => _.concat(module.files.sort(sorter), module.file))
       // Remove duplicates keeping only first occurrences
